8782200c6b3d012126d481dc628c4511ce500843
James McNeil <mcneil_james@hotmail.com>
2023-02-13 21:53:54 +0000

    created userModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('user', userSchema)
