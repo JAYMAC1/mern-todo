ef06ba413a1720d60b8625744e83ef94fa3fa513
James McNeil <mcneil_james@hotmail.com>
2023-02-15 21:29:17 +0000

    created Frontend react app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className='App'>
+      <h3>Clean APP</h3>
+    </div>
+  )
+}
+
+export default App
